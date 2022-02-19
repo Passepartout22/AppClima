467da56cae762a94c894b0c360954369de53f5b5
zeus <emmanuelcach6@gmail.com>
2022-02-19 13:59:36 -0500

    Aplicando diseño y creando una nueva vista

---
+++ app/src/main/java/com/reeze/appclima/Ciudad.kt
@@ -0,0 +1,14 @@
+package com.reeze.appclima
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class Ciudad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        }
+    }
+
