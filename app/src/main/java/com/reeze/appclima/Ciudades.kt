b06a32a823850b723672c04267d5149f01fc4e00
zeus <emmanuelcach6@gmail.com>
2022-03-06 00:32:52 -0500

    Integrando api y añadiendo datos o id de ciudades, creando botones nuevos

--- app/src/main/java/com/reeze/appclima/Ciudades.kt
+++ app/src/main/java/com/reeze/appclima/Ciudades.kt
@@ -9,32 +9,66 @@ import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
 
-    val TAG = "com.reeze.appclima.ciudades.CIUDAD"
+    val TAG = "com.cbtis.appclima.ciudades.CIUDAD"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
 
-        val bFcp = findViewById<Button>(R.id.bFcp)
-        val bMexico = findViewById<Button>(R.id.bMexico)
+        val bMexico = findViewById<Button>(R.id.bMexico) //Shanghai
         val bBerlin = findViewById<Button>(R.id.bBerlin)
+        val bChetumal = findViewById<Button>(R.id.bChetumal)
+        val bFcp = findViewById<Button>(R.id.bFcp)
+        val bMorelos = findViewById<Button>(R.id.bMorelos)
+        val bTulum = findViewById<Button>(R.id.bTulum)
+        val bShanghai = findViewById<Button>(R.id.bShanghai)
 
-        bFcp.setOnClickListener(View.OnClickListener {
-            val intent = Intent( this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-fcp")
+
+        bMexico.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "3530597")
             startActivity(intent)
         })
 
-        bMexico.setOnClickListener(View.OnClickListener {
-            val intent = Intent( this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-mexico")
+        bBerlin.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "2950159")
             startActivity(intent)
         })
 
-        bBerlin.setOnClickListener(View.OnClickListener {
-            val intent = Intent( this, MainActivity::class.java)
-            intent.putExtra(TAG, "ciudad-berlin")
+        bChetumal.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "3531023")
             startActivity(intent)
         })
+
+
+        bMorelos.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "3995398")
+            startActivity(intent)
+        })
+
+        bTulum.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "3515040")
+            startActivity(intent)
+        })
+
+        bTulum.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "3527639")
+            startActivity(intent)
+        })
+
+        bShanghai.setOnClickListener (View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(TAG, "1796236")
+            startActivity(intent)
+        })
+
+
+
+
     }
 }
--- app/src/main/java/com/reeze/appclima/MainActivity.kt
+++ app/src/main/java/com/reeze/appclima/MainActivity.kt
@@ -2,8 +2,14 @@ package com.reeze.appclima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,33 +23,47 @@ class MainActivity : AppCompatActivity() {
 
         tvCiudad = findViewById(R.id.tvCiudad)
         tvGrados = findViewById(R.id.tvGrados)
-        tvEstatus = findViewById(R.id.tvEstado)
+        tvEstatus = findViewById(R.id.tvEstatus)
 
-        val ciudad = intent.getStringExtra("com.reeze.appclima.ciudades.CIUDAD")
 
-        val ciudadfc = com.reeze.appclima.Ciudad("Felipe Carrillo Puerto", grados = 15, estatus = "soleado")
-        val ciudadmx = com.reeze.appclima.Ciudad("Ciudad de Mexico", grados = 30, estatus = "Cielo despejado")
-        val ciudadbe= com.reeze.appclima.Ciudad("Ciudad de Berlin", grados = 5, estatus = "Cielo nublado")
+        val ciudad = intent.getStringExtra("com.cbtis.appclima.ciudades.CIUDAD")
 
-        if(ciudad == "ciudad-fcp"){
-            tvCiudad?.text = ciudadfc.nombre
-            tvGrados?.text = ciudadfc.grados.toString() + "°"
-            tvEstatus?.text = ciudadfc.estatus
 
-        } else if(ciudad == "ciudad-mexico"){
+        if(Network.hayRed(this)){
+            // ejecutar solicitud HTTP
+        solicitudHTTPVolley("https://api.openweathermap.org/data/2.5/weather?id="+ciudad+"&appid=2dc9c9cad42415e73767d224608395ed&units=metric&lang=es")
 
-            tvCiudad?.text = ciudadmx.nombre
-            tvGrados?.text = ciudadmx.grados.toString() + "°"
-            tvEstatus?.text = ciudadmx.estatus
-
-        } else if(ciudad == "ciudad-berlin"){
-
-            tvCiudad?.text = ciudadbe.nombre
-            tvGrados?.text = ciudadbe.grados.toString() + "°"
-            tvEstatus?.text = ciudadbe.estatus
-
-        } else {
-            Toast.makeText(this, "Upps, al parecer no se encontra la información o no exíste", Toast.LENGTH_SHORT).show()
+        }else{
+            // mostrar mensaje de error
+            Toast.makeText(this, "No hay red", Toast.LENGTH_SHORT).show()
         }
+            /*
+        Toast.makeText(this, ciudad, Toast.LENGTH_SHORT).show()//en caso de error mover
+
+            */
     }
+
+    private fun solicitudHTTPVolley(url: String){
+        val queue = Volley.newRequestQueue(this)
+
+        val solicitud = StringRequest(Request.Method.GET, url, {
+            response ->
+            try {
+                Log.d("solicitudHTTPVolley", response)
+
+                val gson = Gson ()
+
+                val ciudad = gson.fromJson(response, Ciudad::class.java)
+                tvCiudad?.text = ciudad.name
+                tvGrados?.text = ciudad.main?.temp.toString()+"°"
+                tvEstatus?.text = ciudad.weather?.get(0)?.description
+
+            }catch (e: Exception){
+
+            }
+        }, { })
+                queue.add(solicitud)
+    }
+
+
 }
